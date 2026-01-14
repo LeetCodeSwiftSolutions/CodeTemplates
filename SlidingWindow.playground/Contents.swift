aac47fc387264c398b26f183cdfb72f89a171ba6
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2026-01-13 22:00:05 -0600

    Actualización de comentarios
    Se mejoró la explicación del algoritmo de acuerdo con el último problema resuelto

--- SlidingWindow.playground/Contents.swift
+++ SlidingWindow.playground/Contents.swift
@@ -2,22 +2,23 @@
 
 func slidingWindow(_ arr: [Int]) -> Int {
     var left = 0
-    var ans = 0
-    var curr = 0
+    var answer = 0
+    var current = 0
     let WINDOW_CONDITION_BROKEN = true
     
     for right in 0..<arr.count {
-        // Debemos analizar el arreglo para ver si agregamos arr[right] a curr
+        // con el desplazamiento de right, actualizamos el valor de "current", con los valores que se agregan a la ventana por la derecha
         
+        // Si se encuentra una ventana válida con el movimiento de right, entramos a este ciclo while
         while(WINDOW_CONDITION_BROKEN) {
-            // Eliminamos arr[left] de curr
+            // Desplazamos left para buscar soluciones óptimas, a partir de la base que nos dejó el desplazamiento de right en el ciclo en que la ventana se hizo válida.
             left += 1
         }
         
-        // Actualizamos ans
+        // Actualizamos "answer" ya sea dentro o fuera del ciclo while.
     }
     
-    return ans
+    return answer
 }
 
 /// Ejercicios que se resuelven con este algoritmo (lista de Sliding Window de mi perfil de LeetCode)
